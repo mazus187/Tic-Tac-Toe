3f587d7d0435f12d6ea1160072a6770089d0e0eb
mazus187 <153563164+mazus187@users.noreply.github.com>
2023-12-18 21:58:23 +0100

    implemented UI in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,85 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean gameRunning = true;
-        while(gameRunning){
+public class GameWithUI extends JFrame {
+    private char aPlayerXorO;//Variable holds the value of the current Player
+    private JButton[] aButtons;//Declarates Buttons
 
-            String[][] spielFeld = {{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
+    public GameWithUI() {
+        setTitle("Tic-Tac-Toe  Player versus Player"); //Title of the Window
+        setSize(getScreenDimension()); //Set the size of the window to the size of the screen
 
-            boolean finished = false;
-            String player = "X";
+        aPlayerXorO = 'X'; //Player "X" begins
+        aButtons = new JButton[9];  //creates Buttons
 
-            do {
-                if (player == "X") {
-                    player = "O";
+        setLayout(new GridLayout(3, 3)); //Set the Gridlayout for the game table
+        setDefaultCloseOperation(EXIT_ON_CLOSE);//End the Application if the window is closed
+
+        initializeButtons();//Initialize the play buttons
+
+        setExtendedState(MAXIMIZED_BOTH); // maximize window
+
+        setVisible(true);
+    }
+    private Dimension getScreenDimension() { //Function to get the screen size of the current
+        return Toolkit.getDefaultToolkit().getScreenSize(); //library operation to get the screen size
+    }
+    public void initializeButtons() {//Initialize the buttons
+        for (int i = 0; i <= 8; i++) {
+            aButtons[i] = new JButton();//create new button
+            aButtons[i].setText("");//set the text of the button
+            aButtons[i].setFont(new Font("Arial", Font.BOLD, 70));  // set the font size for the text
+            aButtons[i].setFocusable(false);//The method specifies wheter the button can be selected or react to user inputs
+            aButtons[i].addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent event) {//the method actionPerformed() is part of the ActionListener Interfaced, it handels events involving buttons and others
+                    JButton buttonClicked = (JButton) event.getSource();
+                    buttonClicked.setText(String.valueOf(aPlayerXorO));
+                    buttonClicked.setEnabled(false);
+
+                    if(gameDone()){
+                        //if the game ends the players can decide to play another round, without restarting the whole Application, a pop up lets the player decide
+                        int option = JOptionPane.showConfirmDialog(null, "Player " + aPlayerXorO + " won! Would you like to play again?", "Game Over", JOptionPane.YES_NO_OPTION);
+                        if(option == JOptionPane.YES_OPTION){//in case they want to play another round, the game table gets resetted
+                            resetButtons();//resets the game table
+                        } else {
+                            System.exit(0);//ends the application
+                        }
+                    }else{
+                        // Switch Players
+                        switchPlayer();
+                    }
                 }
-                else {
-                    player = "X";
-                }
-                drawArray(spielFeld);
-                playerInput(spielFeld, player);
-
-            } while (!gameDone(spielFeld, player));
-
-            drawArray(spielFeld);
-
-            System.out.println("Replay Game? Y/N");
-            String repeatGame = scanner.next();
-            if(!repeatGame.equalsIgnoreCase("Y")){
-                gameRunning = false;
-            }
-
+            });
+            add(aButtons[i]);//Adds the button
         }
-
     }
-
-    private static boolean gameDone(String[][] spielFeld, String player) {
-
-        for (int i = 0; i < 3; i++) {
-            if (spielFeld[i][0].equals(player) && spielFeld[i][1].equals(player) && spielFeld[i][2].equals(player)) {
-                System.out.println("Player " + player + " won!");
+    public boolean gameDone() {
+        // Check horizontal lines.
+        for (int i = 0; i < 9; i += 3)
+            if (checkLine(i, i + 1, i + 2))
                 return true;
-            }
-        }
-        for (int i = 0; i < 3; i++) {
-            if (spielFeld[0][i].equals(player) && spielFeld[1][i].equals(player) && spielFeld[2][i].equals(player)) {
-                System.out.println("Player " + player + " won!");
+
+        // Check vertical lines.
+        for (int i = 0; i < 3; ++i)
+            if (checkLine(i, i + 3, i + 6))
                 return true;
-            }
+    }
+
+    public boolean checkLine(int a, int b, int c) {//checks the line wheter all three buttons are occupied by one user
+        return aButtons[a].getText().equals(aButtons[b].getText()) && aButtons[b].getText().equals(aButtons[c].getText()) && !aButtons[a].getText().equals("");
+    }
+
+    public void switchPlayer() {//Method to switch players
+        aPlayerXorO = (aPlayerXorO == 'X') ? 'O' : 'X';//if the current player is "X" it changes to "O" and the other way around
+    }
+    private void resetButtons(){//method to reset the text of the buttons, as well as reenable them
+        for(int i = 0; i < 9; i++) {
+            aButtons[i].setText("");
+            aButtons[i].setEnabled(true);
         }
-        
-
-        return false;
-
+        aPlayerXorO = 'X';  // Set 'X' as starting player for the next game
     }
-    private static String[][] playerInput(String[][] spielFeld, String player) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Player " + player + ":");
-        System.out.print("X:");
-        int p1_input_x = playerInput("X");
-        System.out.print("Y:");
-        int p1_input_y = playerInput("Y");
-        spielFeld[p1_input_x][p1_input_y] = player;
-        return spielFeld;
-    }
-    private static void drawArray(String[][] array) {
-        System.out.println("   0  1  2");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(i + " ");
-            for (int j = 0; j < array[0].length; j++) {
-                System.out.print("[" + array[i][j] + "]");
-            }
-            System.out.println();
-        }
-    }
-    private static int playerInput(String lineOrColumn){
-    Scanner scanner = new Scanner(System.in); //Initialize Scanner
-    while(true){ //The While statement runs indefinetly and breaks only if the player puts in the correct values
-        int entry = scanner.nextInt();//Scans player input
-        if(0<=entry && entry<=2){//Checks the input for the correct value
-            return entry;
-        }else{
-            System.out.println("Only values between 0 and 2 are permitted, please write the value for "+ lineOrColumn+" again"); //Message with explenation incase of an wrong input
-        }
-    }
-
 }
-}
-
-
